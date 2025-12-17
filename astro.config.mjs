0ac5f243225e548c6a262f8ea96a528ad59b6e94
SARAVN <saravn.ent@gmail.com>
2025-12-17 10:13:08 +0530

    feat: Initialize new Astro website with comprehensive transportation services, components, and assets.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://kalidasstravels.in',
   integrations: [
     tailwind(),
     react(),
+    sitemap(),
   ],
 });
